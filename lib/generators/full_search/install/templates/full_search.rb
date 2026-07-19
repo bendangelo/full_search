4b6209ebca326821cc3fa364d41f6b5b36d61ecb
Ben D'Angelo <ben@bendangelo.me>
2026-07-19 16:54:27 -0400

    feat: enable auto_rebuild_on_stale_query by default in local environments

--- lib/generators/full_search/install/templates/full_search.rb
+++ lib/generators/full_search/install/templates/full_search.rb
@@ -3,6 +3,8 @@
 FullSearch.configure do |config|
   config.auto_rebuild_schema = Rails.env.local?
 
+  config.auto_rebuild_on_stale_query = Rails.env.local?
+
   config.stale_query_behavior = Rails.env.production? ? :log_and_fallback : :raise
 
   config.lock_rebuilds = true
